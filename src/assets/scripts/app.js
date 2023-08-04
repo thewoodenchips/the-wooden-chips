100ee6c2b838b4c78a151a22ca72b91eea967ad7
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2023-08-03 21:07:02 -0400

    fixed navbar apearing prematurely

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,5 +1,40 @@
 // © Manuel Matuzović: https://web.dev/website-navigation/
 
+
+
+const nav = document.querySelector('nav');
+const list = nav.querySelector('ul');
+const burgerClone = document.querySelector('#burger-template').content.cloneNode(true);
+const svg = nav.querySelector('svg');
+
+const button = burgerClone.querySelector('button');
+button.addEventListener('click', e => {
+  const isOpen = button.getAttribute('aria-expanded') === 'true';
+  button.setAttribute('aria-expanded', !isOpen);
+});
+
+// avoid DRY: disabling menu
+const disableMenu = () => {
+  button.setAttribute('aria-expanded', false);
+};
+
+//  close on escape
+nav.addEventListener('keyup', e => {
+  if (e.code === 'Escape') {
+    disableMenu();
+  }
+});
+
+// close if clicked outside of event target
+document.addEventListener('click', e => {
+  const isClickInsideElement = nav.contains(e.target);
+  if (!isClickInsideElement) {
+    disableMenu();
+  }
+});
+
+nav.insertBefore(burgerClone, list);
+
 function Gallery(gallery) {
   if (!gallery) {
     throw new Error("No Gallery Found!");
@@ -84,43 +119,3 @@ function Gallery(gallery) {
   modal.addEventListener("click", handleClickOutside);
 }
 
-
-const nav = document.querySelector('nav');
-const list = nav.querySelector('ul');
-const burgerClone = document.querySelector('#burger-template').content.cloneNode(true);
-const svg = nav.querySelector('svg');
-
-const button = burgerClone.querySelector('button');
-button.addEventListener('click', e => {
-  const isOpen = button.getAttribute('aria-expanded') === 'true';
-  button.setAttribute('aria-expanded', !isOpen);
-});
-
-// Use image gallery on the page
-
-const gallery = Gallery(document.querySelector(".declare-gallery"));
-
-
-
-
-// avoid DRY: disabling menu
-const disableMenu = () => {
-  button.setAttribute('aria-expanded', false);
-};
-
-//  close on escape
-nav.addEventListener('keyup', e => {
-  if (e.code === 'Escape') {
-    disableMenu();
-  }
-});
-
-// close if clicked outside of event target
-document.addEventListener('click', e => {
-  const isClickInsideElement = nav.contains(e.target);
-  if (!isClickInsideElement) {
-    disableMenu();
-  }
-});
-
-nav.insertBefore(burgerClone, list);
